1f33fdbb9725f52717ea8c93f7f2af5516362844
ea <aboajah.emmanuel@fmdqgroup.com>
2023-01-12 11:23:50 +0100

    route

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -81,7 +81,7 @@ const Footer = () => {
   return (
     <Container>
       <Left>
-        <Logo>LAMA.</Logo>
+        <Logo>MANNY.</Logo>
         <Desc>
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
