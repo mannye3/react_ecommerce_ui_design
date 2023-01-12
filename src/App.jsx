292e5e498b315ea727297980d75aa075d0e617c8
ea <aboajah.emmanuel@fmdqgroup.com>
2023-01-12 10:51:10 +0100

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+import Home from "./pages/Home";
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+import Register from "./pages/Register";
+//import Product from './componets/Product';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
+
+import { Route, Routes } from "react-router-dom"
+
+
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<ProductList />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cart" element={<Cart />} />
+      
+    </Routes>
+  )
+}
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Announcement from '../componets/Announcement'
+import Categories from '../componets/Categories'
+import Footer from '../componets/Footer'
+import Navbar from '../componets/Navbar'
+import Newsletter from '../componets/Newsletter'
+import Products from '../componets/Products'
+import Sliders from '../componets/Sliders'
+
+const Home = () => {
+  return (
+    <div>
+        <Announcement/>
+     <Navbar/>
+     <Sliders/>
+     <Categories/>
+     <Products/>
+     <Newsletter/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default Home
